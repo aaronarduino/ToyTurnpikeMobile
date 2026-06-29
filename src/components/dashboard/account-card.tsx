59d19f98f5935d53cbd04e7a1f102c0fbeae09b0
Aaron Alexander <aarona.arduino@gmail.com>
2026-06-29 14:14:52 -0500

    feat: start to refactor out the styles to the theme constants file

--- src/components/dashboard/account-card.tsx
+++ src/components/dashboard/account-card.tsx
@@ -1,36 +1,19 @@
-import * as Device from "expo-device";
 import { authClient } from "@/lib/auth-client";
-import {
-  Button,
-  Platform,
-  Pressable,
-  ScrollView,
-  StyleSheet,
-  Text,
-} from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Button } from "react-native";
 import { User } from "lucide-react-native";
-import { ExternalLink } from "@/components/external-link";
 
-import { AnimatedIcon } from "@/components/animated-icon";
 import { HintRow } from "@/components/hint-row";
 import { ThemedText } from "@/components/themed-text";
 import { ThemedView } from "@/components/themed-view";
-import { WebBadge } from "@/components/web-badge";
-import { useSession } from "@/ctx";
-import { BottomTabInset, MaxContentWidth, Spacing } from "@/constants/theme";
-import { useEffect, useState } from "react";
-import { useTheme } from "@/hooks/use-theme";
-import { formatter } from "@/constants/currency";
+import { Styles } from "@/constants/theme";
 import { lastEightOf } from "@/lib/utils";
 import { router } from "expo-router";
 
 export default function AccountCard() {
   const { data: session } = authClient.useSession();
-  const theme = useTheme();
 
   return (
-    <ThemedView type="backgroundElement" style={styles.card}>
+    <ThemedView type="backgroundElement" style={Styles.card}>
       <ThemedView
         type="backgroundElement"
         style={{ borderBottomWidth: 1, borderBottomColor: "#c4c4d1" }}
@@ -47,7 +30,7 @@ export default function AccountCard() {
         </ThemedText>
       </ThemedView>
 
-      <ThemedView type="backgroundElement" style={styles.cardContent}>
+      <ThemedView type="backgroundElement" style={Styles.cardContent}>
         <HintRow
           title="Account Holder"
           hint={<ThemedText type="code">{session?.user.name}</ThemedText>}
@@ -66,24 +49,3 @@ export default function AccountCard() {
     </ThemedView>
   );
 }
-
-const styles = StyleSheet.create({
-  card: {
-    gap: Spacing.three,
-    alignSelf: "stretch",
-    paddingVertical: Spacing.three,
-    borderRadius: Spacing.three,
-  },
-  cardContent: {
-    gap: Spacing.three,
-    alignSelf: "stretch",
-    paddingHorizontal: Spacing.three,
-  },
-  stepContainer: {
-    gap: Spacing.three,
-    alignSelf: "stretch",
-    paddingHorizontal: Spacing.three,
-    paddingVertical: Spacing.four,
-    borderRadius: Spacing.four,
-  },
-});
